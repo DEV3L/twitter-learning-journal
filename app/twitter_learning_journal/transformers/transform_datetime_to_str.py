de3f841ae5417b1f700e302b81d1d3a7114296b8
Justin Beall <jus.beall@gmail.com>
2017-12-19 23:09:02 -0500

    Add transform_datetime_to_str transformer

---
+++ app/twitter_learning_journal/transformers/transform_datetime_to_str.py
@@ -0,0 +1,2 @@
+def transform_datetime_to_iso_date_str(input_datetime):
+    return input_datetime.date().isoformat()
---
+++ tests/twitter_learning_journal/transformers/test_transform_datetime_to_str.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+from app.twitter_learning_journal.transformers.transform_datetime_to_str import transform_datetime_to_iso_date_str
+
+
+def test_transform_datetime_to_iso_date_str():
+    input_datetime = datetime(2017, 12, 19)
+
+    assert '2017-12-19' == transform_datetime_to_iso_date_str(input_datetime)
