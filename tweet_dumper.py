5326df37f43a787b70fa4836599d296a90816529
Justin Beall <jus.beall@gmail.com>
2017-12-19 22:59:52 -0500

    Add favorite_dao

--- tweet_dumper.py
+++ tweet_dumper.py
@@ -10,9 +10,10 @@ It has been stitched together from a few github gist files
 # https://gist.github.com/yanofsky/5436496
 # https://gist.github.com/datagrok/74a71f572493e603919e
 
+from app.twitter_learning_journal.dao.favorite_dao import FavoriteDao
 from app.twitter_learning_journal.database.sqlalchemy_database import Database
 from app.twitter_learning_journal.models import Base
-from app.twitter_learning_journal.models.favorite import Favorite
+from app.twitter_learning_journal.services.favorites_processing_service import FavoritesProcessingService
 from app.twitter_learning_journal.twitter_api.api import get_api
 from app.twitter_learning_journal.twitter_api.favorites import Favorites
 
@@ -52,10 +53,6 @@ def get_all_tweets(screen_name):
     #
     # # transform the tweepy tweets into a 2D array that will populate the csv
     # outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
-
-    favorites = Favorites(api, screen_name)
-    favorites_list = favorites.get()
-
     # # write the csv
     # with open('%s_tweets.csv' % screen_name, 'w') as f:
     #     writer = csv.writer(f)
@@ -65,16 +62,27 @@ def get_all_tweets(screen_name):
     database = Database()
     Base.metadata.create_all(database._engine)
 
+    favorite_dao = FavoriteDao(database)
+
+    favorites = Favorites(api, screen_name)
+    favorites_list = favorites.get()
+
     for favorite in favorites_list:
-        _favorite = database.query(Favorite).filter(Favorite.id == favorite.id).first()
+        if not favorite_dao.exists(favorite.id):
+            favorite_dao.add(favorite)
 
-        if not _favorite:
-            database.add(favorite)
-
-    # sql_favorites_list = database.query(Favorite).all()
-    # for favorite in sql_favorites_list:
-    #     print(favorite)
+    _favorites = favorite_dao.query_all()
 
     database.commit()
+
+    favorites_processing_service = FavoritesProcessingService(_favorites)
+    favorites_processing_service.count_words()
+
+    favorite_dao.add_all(favorites_processing_service.favorites)
+    database.commit()
+
+    _favorites = favorite_dao.query_all()
+    for favorite in _favorites:
+        print(favorite)
 
     return favorites_list
