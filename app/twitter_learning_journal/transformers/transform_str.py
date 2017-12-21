6b7981557227db7bd0a4d776a83f49789f197fc2
Justin Beall <jus.beall@gmail.com>
2017-12-20 22:48:28 -0500

    Add tokenize str transformation

--- app/twitter_learning_journal/transformers/transform_str.py
+++ app/twitter_learning_journal/transformers/transform_str.py
@@ -5,3 +5,7 @@ def remove_ignore_characters_from_str(input_str, replacement=' '):
     for ignore_character in ignore_characters:
         input_str = input_str.replace(ignore_character, replacement)
     return input_str
+
+
+def tokenize(input_str, *, delimiter=None):
+    return input_str.split(delimiter) if input_str else []
--- tests/twitter_learning_journal/transformers/test_transform_str.py
+++ tests/twitter_learning_journal/transformers/test_transform_str.py
@@ -1,5 +1,5 @@
 from app.twitter_learning_journal.classifiers import ignore_characters
-from app.twitter_learning_journal.transformers.transform_str import remove_ignore_characters_from_str
+from app.twitter_learning_journal.transformers.transform_str import remove_ignore_characters_from_str, tokenize
 
 
 def test_remove_ignore_characters_from_str():
@@ -11,3 +11,15 @@ def test_remove_ignore_characters_from_str_with_replacement():
     replacement = '|'
     for character in ignore_characters:
         assert replacement == remove_ignore_characters_from_str(character, replacement=replacement)
+
+
+def test_tokenize():
+    test_cases = [
+        ('', None, []),
+        (' ', None, []),
+        ('a b', None, ['a', 'b']),
+        ('a|b', '|', ['a', 'b']),
+    ]
+
+    for input_str, delimiter, expected_value in test_cases:
+        assert expected_value == tokenize(input_str, delimiter=delimiter)
