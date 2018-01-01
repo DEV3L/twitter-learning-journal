c88c9775f0c9e6e97de82ff4d5fb3e028371a3e5
Justin Beall <jus.beall@gmail.com>
2018-01-01 12:09:40 -0500

    Break down run to be moved to Flask endpoint

--- run.py
+++ run.py
@@ -1,358 +1,264 @@
-# from urllib import request
-from urllib import request
-from urllib.request import Request
-
-from bs4 import BeautifulSoup
+from datetime import datetime
 
 from app.twitter_learning_journal.dao.tweet_dao import TweetDao
 from app.twitter_learning_journal.database.sqlalchemy_database import Database
 from app.twitter_learning_journal.models.detail import Detail
-from app.twitter_learning_journal.transformers.transform_str import remove_ignore_characters_from_str
 from app.twitter_learning_journal.twitter_api.api import get_api
+from scripts.audio_books import get_audio_books
+from scripts.blogs import count_html_words
+from scripts.books import get_books
+from scripts.podcasts import count_podcast_words
 from scripts.trainers.detail_trainer import train_details
-from tweet_dumper import timeline, collect, save_tweets, build_tables, classify_tweets, classify_audible_books, \
+from tweet_dumper import collect, save_tweets, build_tables, classify_tweets, \
     add_sub_classification_to_models
 
 
-def clean_me(html):
-    soup = BeautifulSoup(html)
-    for s in soup(['script', 'style', 'head']):
-        s.decompose()
-    return ' '.join(soup.stripped_strings)
-
-
-podcast_default_minutes = 20
-
-# https://www.quora.com/Speeches-For-the-average-person-speaking-at-a-normal-pace-what-is-the-typical-number-of-words-they-can-say-in-one-minute
-# "publishers recommend books on tape to be voiced at 150-160 wpm"
-podcast_words_per_minute = 125  # well below stated suggestion
-
-
-def count_podcast_words():
-    database = Database(echo=False)
-    tweet_dao = TweetDao(database)
-    details = tweet_dao._database.query(Detail).all()
-
-    # share url from pocket cast has 'pca.st' in it
-    details = [detail for detail in details if detail.url and
-               [url for url in detail.url.split('|') if 'pca.st' in url]
-               ]
-
-    for detail in details:
-        if detail.is_fully_classified:
-            continue
-
-        detail.type = 'podcast'
-        minutes = podcast_default_minutes
-
-        for url in detail.url.split('|'):
-            if 'pca.st' not in url:
-                continue
-
-            if url in podcast_urls_and_times:
-                minutes = podcast_urls_and_times[url]
-                detail.is_fully_classified = True
-            else:
-                detail.is_fully_classified = False
-                print(url)
-
-        detail.word_count = minutes * podcast_words_per_minute
-        database.commit()
-
-
-podcast_urls_and_times = {
-    'http://pca.st/Z0Kh': 42,
-    'http://pca.st/9GMB': 78,
-    'http://pca.st/Ha6R': 49,
-    'http://pca.st/xJ9C': 66,
-    'http://pca.st/Rx7b': 50,
-    'http://pca.st/fp1m': 59,
-    'http://pca.st/l0il': 59,
-    'http://pca.st/X8me': 54,
-    'http://pca.st/shSo': 48,
-    'http://pca.st/Lc6p': 25,
-    'http://pca.st/H4Ym': 56,
-    'http://pca.st/7zSP': 30,
-    'http://pca.st/8TLM': 20,
-    'http://pca.st/uRCi': 50,
-    'http://pca.st/q9RK': 57,
-    'http://pca.st/nN91': 62,
-    'http://pca.st/7037': 39
-
-}
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) '
-                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36'
-}
-
-def count_html_words():
+def classify_other_details():
     database = Database()
     tweet_dao = TweetDao(database)
     details = tweet_dao._database.query(Detail).all()
-
-    print(f'total details: {len(details)}')
-    detail_count = 1
+    details = [detail for detail in details if detail.type == 'other']
 
     for detail in details:
-        if not detail.url:
-            pass
+        print(detail)
+        print()
 
-        total_words = 0
+    pass
 
-        for url in detail.url.split('|'):
-            is_blog = True
 
-            for video_url in not_blog_urls:
-                if video_url in url:
-                    print(f'@@@@ URL looks like not blog: {url}')
-                    detail.is_fully_classified = False
-                    detail.type = 'other'
-                    database.add(detail)
-                    is_blog = False
+manual_detail_classification = {
 
-            if not is_blog:
-                continue
+}
 
-            url = url.replace('www.google.com/amp/s/', '')
 
-            _request = Request(url)
-            _request.headers = headers
+def classify_keyword_tweets():
+    database = Database(echo=False)
+    tweet_dao = TweetDao(database)
+    details = tweet_dao._database.query(Detail).all()
+    details = [detail for detail in details if detail.type == 'keyword']
+    total = len(details)
+    count = 0
 
-            try:
-                html = request.urlopen(_request).read().decode('utf8')
-            except Exception as e:
-                print(f'could not open url: {url}')
-                continue
+    for detail in details:
+        count += 1
 
-            html = clean_me(html)
+        _title = detail.title.strip()
 
-            _raw = BeautifulSoup(html).get_text()
-            _raw = remove_ignore_characters_from_str(_raw)
-            _raw = ' '.join(_raw.split())
+        print(f'{detail.start_date}:{_title}')
 
-            raw_split = _raw.split()
-            words = len(raw_split)
-
-            print(url)
-            print(words)
-
-            devation = default_domain_devation
-            is_found = False
-
-            if words == 0:
-                print('Removed/Redirected URL')
-                continue
-
-            # I feel like this is a common problem that could be optimized
-            for key in domain_deviations.keys():
-                if key in url:
-                    is_found = True
-                    devation = domain_deviations[key]
-
-            if not is_found:
-                print()
-
-            if words > (devation * 1.25):
-                words = words - devation
-
-            print(f'counted words: {words}')
-            total_words += words
-
-        detail_count += 1
-
-        print(f'processed: {detail_count}')
-
-        if total_words == 0:
+        if _title in manual_keyword_detail_classification.keys():
             continue
 
-        detail.word_count = total_words
-        database.add(detail)
-    database.commit()
-    print()
+        is_continue = False
+        for key in manual_keyword_detail_classification.keys():
+            if key in _title:
+                is_continue = True
+                break
+
+        if is_continue:
+            continue
+
+    pass
 
 
-default_domain_devation = 300
+manual_keyword_detail_classification = {
+    'smart guys like ': None,
+    'this is a': None,
+    'great insight on writing clean code from one of the greats': None,
+    'i read this somewhere, seen it today': None,
+    '&gt;&gt;&gt; import this': None,
+    'product reading group': None,
+    'view the code as a reader, not as an author': None,
+    '#readinggroup @onshift': None,
+    'building and building without listening': None,
+    'i sympathy read it with a junior dev': None,
+    'product reading from': None,
+    'product reading group tom': None,
+    'looking forward to reading it': None,
+    'maybe on his/her commute in to work': None,
+    'are trying to connect to the onagile conference': None,
+    'truly great developers are difficult to find': None,
+    'is to immediately roll back': None,
+    'i run lean coffee': None,
+    'i am halfway through': None,
+    'literature i read': None,
+    'i wish founders would read': None,
+    'current status:': None,
+    'how many #agile practitioners and coaches have actually read the agile manifesto?': None,
+    'thinking about accounting for audio books next': None,
+    '@johncutlefish interesting project!': None,
+    'oh this makes me happy': None,
+    '#leancoffee topics': None,
+    'thank you for sharing': None,
+    'what other lean books have you read': None,
+    'looking forward to an exciting read over': None,
+    'a must watch for all devops leaders': None,
+    'some people really benefit from hearing advice that everyone knows': None,
+    'i believe i saw in the thread': None,
+    'if you have to do something manually more than once, automate it so you never have to do it again': None,
+    '#aatc2017 #onshift': None,
+    'finished listening to the phoenix project on #audible, thanks @realgenekim! #pheonixproject': None,  # mistweet
 
-domain_deviations = {
-    'cucumber.io': 200,
-    'blog.jessitron.com': 250,
-    '12factor.net': 25,
-    'interviewcake.com': 550,
-    'yanado.com': 300,
-    'medium.freecodecamp.org': 200,
-    'deque.blog': 300,
-    'shmula.com': 500,
-    'fearlesssalarynegotiation.com': 500,
-    'gizmodo.com': 150,
-    'expertenough.com': 150,
-    'github.io': 100,
-    'employeebenefitadviser.com': 250,
-    'pragprog.com': 150,
-    'treyhunner.com': 500,
-    'scientistlive.com': 700,
-    'fastcodesign.com': 350,
-    'caroli.org': 250,
-    'Caroli.org': 250,
-    'coreyhaines.com': 700,
-    'tjelvarolsson.com': 150,
-    'aits.org': 400,
-    'techbeacon.com': 1700,
-    'curl.trillworks.com': 25,
-    'oswalpalash.com': 25,
-    'theoatmeal.com': 25,
-    'opensource.googleblog.com': 300,
-    'lean.org': 1050,
-    'nyti.ms': 450,
+    # videos
+    'i would highly recommend watching': ('2017-11-12', 'video', ''),
 
-    'quora.com': 150,
-    'hackernoon.com': 175,
-    'insidebigdata.com': 415,
-    'fcc.im': 250,
-    'lornemitchell.com': 400,
-    'medium.com': 350,
-    'wikipedia.org': 1250,
-    'ASP.NET': default_domain_devation,
-    'visualstudio.com': 1500,
-    'devbridge.com': 200,
-    'buff.ly': 600,
-    'ben-evans.com': 200,
-    'runlean.ly': 200,
-    'dev.to': 200,
-    'agileuprising.com': 200,
-    'techcrunch.com': 100,
-    'tobeagile.com': 225,
-    'scrumalliance.org': 1650,
-    'increment.com': 650,
-    'solutionsiq.com': 550,
-    'wordpress.com': 200,
-    'eleganthack.com': 650,
-    'brodzinski.com': 425,
-    'blog.cleancoder.com': 650,
-    'blog.juandelgado.es': 150,
-    'blackswanfarming.com': 400,
-    'thght.works': 400,
-    'zumsteg.net': 350,
-    'ribbonfarm.com': 1600,
-    'curiosity.com': 250,
-    'blogspot.com': 250,
-    'builttoadapt.io': 150,
-    'm.signalvnoise.com': 250,
-    'mcfunley.com': 200,
-    'dbader.org': 800,
-    'martinfowler.com': 300,
-    'thoughtworks.com': 300,
-    'extremeuncertainty.com': 250,
-    'facebook.com': 100,
-    'michaelnygard.com': 150,
-    'vitsoe.com': 150,
-    'a16z.com': 150,
-    'engineering.semantics3.com': 200,
-    'blog.coffeeandcode.com': 150,
-    'motherboard.vice.com': 150,
-    'andrewchen.co': 150,
-    'inc.com': 200,
-    'startupsventurecapital.com': 200,
-    'michaelfeathers.silvrback.com': 50,
-    'blog.wingman-sw.com': 2000,
-    'nataliewarnert.com': 350,
-    'solutionsiq.in': 225,
-    'itrevolution.com': 400,
-    'linkedin.com': 50,
-    'lnkd.in': 50,
-    'mountaingoatsoftware.com': 500,
-    'producthabits.com': 150,
-    'keybase.io': 250,
-    'agilealliance.org': 350,
-    'codemash.org': 250,
-    'stackoverflow.com': 200,
-    'medium': 200,
-    'fastcompany.com': 450,
-    'blog.thedigitalcatonline.com': 750,
-    'sandimetz.com': 150,
-    'stickyminds.com': 400,
-    'laughingmeme.org': 500,
-    'ronjeffries.com': 250,
-    'luis-goncalves.com': 850,
-    'lucidchart.com': 200,
-    'simpleisbetterthancomplex.com': 300,
-    'jasonrudolph.com': 150,
-    'engineering.onshift.com': 125,
-    'eng.lyft.com': 100,
-    'spikesandstories.com': 500,
-    'labs.spotify.com': 1800,
-    'lifehacker.com': 300,
-    'wired.com': 550,
-    'meowni.ca': 300,
-    'microsoft.com': 500,
-    'svpg.com': 200,
-    'worldpositive.com': 1200,
-    'aws.amazon.com': 4700,
+    # ###
+    # audio books
+    # ###
 
-    'leanqa.wordpress.com': 0,
+    # making work visible
+    'started listening to: making work visible': ('2017-12-11', 'audio book', 'start'),
+    'finished listening to: making work visible': ('2017-12-16', 'audio book', 'stop'),
 
-    'snap-ci.com': default_domain_devation,
-    'sumo.ly': default_domain_devation,
-    'bit.ly': default_domain_devation,
-    'shar.es': default_domain_devation,
-    'go.shr.lc': default_domain_devation,
-    'dlvr.it': default_domain_devation,
-    'getpocket.com': default_domain_devation,
-    'leanuxmas.com': default_domain_devation,
-    'fb.me': default_domain_devation,
-    'tinyurl.com': default_domain_devation,
-    'ow.ly': default_domain_devation,
+    # start with why
+    'started listening to start with why': ('2017-11-27', 'audio book', 'start'),
+    "finished listening to 'start with why: how great leaders inspire everyone to take action'":
+        ('2017-12-03', 'audio book', 'stop'),
 
+    # a seat at the table
+    'started listening to a seat at the table': ('2017-10-17', 'audio book', 'start'),  # inferred
+    'finished listening to a seat at the table': ('2017-11-21', 'audio book', 'stop'),
+
+    # lean in
+    'started listening to lean in': ('2017-09-20', 'audio book', 'start'),
+    'finished listening to lean in': ('2017-10-16', 'audio book', 'stop'),
+
+    # the five dysfunctions of a team
+    'started listening to the five dysfunctions of a team': ('2017-09-14', 'audio book', 'start'),
+    'finished listening to the five dysfunctions of a team': ('2017-09-20', 'audio book', 'stop'),
+
+    # the happiness advantage
+    'started listening to the happiness advantage': ('2017-08-19', 'audio book', 'start'),  # inferred
+    'finished listening to the happiness advantage': ('2017-09-14', 'audio book', 'stop'),
+
+    # scrum
+    'started listening to scrum': ('2017-08-02', 'audio book', 'start'),
+    'finished listening to scrum': ('2017-08-18', 'audio book', 'stop'),
+
+    # work rules!
+    'started listening to work rules!': ('2017-07-18', 'audio book', 'start'),
+    'finished listening to work rules!': ('2017-08-02', 'audio book', 'stop'),
+
+    # drive
+    'started listening to drive': ('2017-06-28', 'audio book', 'start'),
+    'finished listening to drive': ('2017-07-17', 'audio book', 'stop'),
+
+    # crucial conversations
+    'began listening to crucial conversations': ('2017-06-12', 'audio book', 'start'),
+    'finished listening to crucial conversations': ('2017-06-25', 'audio book', 'stop'),
+
+    # switch
+    # Started listening to on Audible:\nSwitch: How to Change Things When Change Is Hard
+    'started listening to on audible:': ('2017-05-24', 'audio book', 'start'),
+    'finished listening to switch': ('2017-06-08', 'audio book', 'stop'),
+
+    # the subtle art of not giving a f*ck
+    # Finished listening to on Audible:\nThe Subtle Art of Not Giving a F*ck
+    'started listening to the subtle art of not giving a f*ck': ('2017-04-23', 'audio book', 'start'),  # inferred
+    'finished listening to on audible:': ('2017-05-23', 'audio book', 'stop'),
+
+    # ###
+    # books
+    # ###
+    # 250-300 words per page, 250 words per page, 150-450 words per page -- 3 rando sources
+    # 200
+
+    # cracking the pm interview
+    'started reading: cracking the pm interview': ('2017-12-16', 'book', 'start'),
+    'finished reading: cracking the pm interview': ('2017-12-30', 'book', 'stop'),
+
+    # pages = 1364
+    # wpp = 200
+    # pages * wpp = total_words
+    # total_days = (book.stop_date - book.start_date) + 1
+    # total_days = 15
+    # total_words =
+
+    # how f*cked up is your management
+    'started reading: how f*cked up is your management by @johnath  and @shappy tonight! #techcoach @hfuiym': (
+    '2017-11-11', 'book', 'start'),
+    'finished reading: how f*cked up is your management - an uncomfortable conversation about modern leadership by @hfuiym': (
+    '2017-12-15', 'book', 'stop'),
+
+    # clean coder
+    'started reading the clean code by @unclebobmartin': ('2017-11-15', 'book', 'start'),  # should be clean coder!
+    'finished reading the clean coder': ('2018-02-15', 'book', 'stop'),
+
+    # production ready microservices
+    'began reading production ready microservices by @susanthesquark': ('2017-10-12', 'book', 'start'),
+    'finished reading production ready microservices by @susanthesquark': ('2017-11-02', 'book', 'stop'),
+
+    # head first python
+    'began reading head first python': ('2017-07-26', 'book', 'start'),
+    'finished reading head first python': ('2017-10-03', 'book', 'stop'),
+
+    # the startup way
+    'started reading the startup way': ('2017-08-09', 'book', 'start'),
+    'finished reading the startup way': ('2017-01-03', 'book', 'stop'),
+
+    # mythical man-month
+    'started reading the mythical man-month': ('2017-07-03', 'book', 'start'),
+    'finished reading the mythical man-month': ('2017-08-13', 'book', 'stop'),
+
+    # clean code
+    'started reading clean code': ('2017-06-07', 'book', 'start'),
+    'finished reading clean code': ('2017-08-05', 'book', 'stop'),
+
+    # user story mapping
+    'started reading user story mapping': ('2017-05-14', 'book', 'start'),
+    'finished reading user story mapping': ('2017-05-03', 'book', 'stop'),
+
+    # object thinking
+    'started reading object thinking': ('2017-05-02', 'book', 'start'),
+    'finished reading object thinking': ('2017-05-14', 'book', 'stop'),
+
+    # the pragmatic programmer
+    'started reading the pragmatic programmer': ('2017-03-02', 'book', 'stop'),  # inferred
+    'finished reading the pragmatic programmer': ('2017-05-02', 'book', 'stop'),
 }
-
-not_blog_urls = {
-    'youtu.be',
-    'youtube.com',
-    'vimeo.com',
-
-    'slideshare.net',
-
-    # podcast
-    'pca.st',
-
-    'gist.github.com',
-
-    'meetup.com',
-
-    # conference
-    'sched.co',
-
-    # words out...
-    'dev.to/dev3l',
-    'softwaredev3loper.wordpress.com',
-}
-
 
 if __name__ == '__main__':
     screen_name = 'dev3l_'
+
+    min_date = datetime(year=2017, month=11, day=28)
+    max_date = datetime(year=2017, month=12, day=28, hour=23, minute=59)
 
     database = Database()
     tweet_dao = TweetDao(database)
 
     build_tables(database)
 
+    # extract
     api = get_api()
     tweets = collect(api, screen_name, tweet_type='tweet')
     favorites = collect(api, screen_name)
 
-
     save_tweets(tweet_dao, favorites)
     save_tweets(tweet_dao, tweets)
 
+    # transform
     classify_tweets(tweet_dao)
 
     add_sub_classification_to_models(tweet_dao)
 
     train_details()
 
-    count_podcast_words()
     count_html_words()
+    count_podcast_words()
 
-    books = classify_audible_books()
+    classify_keyword_tweets()
 
-    timeline(tweet_dao, books)
+    # classify_other_details()
+    audio_books = get_audio_books()
+
+    books = []
+    books.extend(get_books())
+
+    tweets = tweet_dao.query_all()
+    _tweets = [tweet for tweet in tweets if tweet.created_at >= min_date and tweet.created_at <= max_date]
+
+    details = tweet_dao._database.query(Detail).all()
+    _details = [detail for detail in details if detail.start_date >= min_date and detail.stop_date <= max_date]
+
+    # load
+    # timeline(min_date, max_date, _tweets, _details, audio_books, books)
