94520c56128f54b218831021e7288b1842272a2e
Justin Beall <jus.beall@gmail.com>
2018-01-14 21:22:08 -0500

    Add Twitter Retriever

---
+++ tests/twitter_learning_journal/retrievers/test_twitter.py
@@ -0,0 +1,14 @@
+from unittest.mock import MagicMock
+
+from app.twitter_learning_journal.retrievers.twitter import TwitterRetriever
+
+
+def test_init_twitter_retriever():
+    mock_api = MagicMock()
+    screen_name = 'screen_name'
+
+    twitter_retriever = TwitterRetriever(mock_api, screen_name)
+
+    assert mock_api == twitter_retriever.api
+    assert screen_name == twitter_retriever.screen_name
+    assert not twitter_retriever.is_favorite
