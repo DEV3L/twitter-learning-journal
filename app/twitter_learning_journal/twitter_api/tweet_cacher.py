d32e387d4c08853d9b26588c209d2d1eec3c5bda
Justin Beall <jus.beall@gmail.com>
2018-01-14 23:00:47 -0500

    Add _init_cache_dir to Tweet Cacher

--- app/twitter_learning_journal/twitter_api/tweet_cacher.py
+++ app/twitter_learning_journal/twitter_api/tweet_cacher.py
@@ -3,8 +3,15 @@ from app.twitter_learning_journal.models.tweet import Tweet
 
 tweet_cache_path = os_environ('TWEET_CACHE_PATH', default='./data/pickle/tweets/')
 
+from os import makedirs, path
 
 class TweetCacher:
-    def __init__(self, tweet: Tweet):
-        self.cache_path = tweet_cache_path
+    def __init__(self, screen_name: str, tweet: Tweet):
+        self.cache_path = f'{tweet_cache_path}{screen_name}'
         self.tweet = tweet
+
+        self._init_cache_dir()
+
+    def _init_cache_dir(self):
+        if not path.isdir(self.cache_path):
+            makedirs(self.cache_path)
--- tests/twitter_learning_journal/twitter_api/test_tweet_cacher.py
+++ tests/twitter_learning_journal/twitter_api/test_tweet_cacher.py
@@ -1,17 +1,43 @@
-from unittest.mock import MagicMock
+from unittest.mock import patch
 
+from app.twitter_learning_journal.models.tweet import Tweet
 from app.twitter_learning_journal.twitter_api.tweet_cacher import TweetCacher
 
+expected_cache_path = './data/pickle/tweets/screen name'
+expected_screen_name = 'screen name'
 
-def test_tweet_cacher_init():
-    expected_cache_path = './data/pickle/tweets/'
-    mock_tweet = MagicMock()
 
-    tweet_cacher = TweetCacher(mock_tweet)
+@patch('app.twitter_learning_journal.twitter_api.tweet_cacher.TweetCacher._init_cache_dir')
+def test_tweet_cacher_init(mock_init_cache_dir):
+    expected_tweet = Tweet()
+
+    tweet_cacher = TweetCacher(expected_screen_name, expected_tweet)
 
     assert expected_cache_path == tweet_cacher.cache_path
-    assert mock_tweet == tweet_cacher.tweet
+    assert expected_tweet == tweet_cacher.tweet
+    assert mock_init_cache_dir.called
 
+
+@patch('app.twitter_learning_journal.twitter_api.tweet_cacher.makedirs')
+@patch('app.twitter_learning_journal.twitter_api.tweet_cacher.path')
+def test_init_cache_dir_exists(mock_path, mock_makedirs):
+    mock_path.isdir.return_value = True
+
+    tweet_catch = TweetCacher(expected_screen_name, Tweet())
+
+    mock_path.isdir.assert_called_with(tweet_catch.cache_path)
+    assert not mock_makedirs.called
+
+
+@patch('app.twitter_learning_journal.twitter_api.tweet_cacher.makedirs')
+@patch('app.twitter_learning_journal.twitter_api.tweet_cacher.path')
+def test_init_cache_dir_not_exists(mock_path, mock_makedirs):
+    mock_path.isdir.return_value = False
+
+    tweet_catch = TweetCacher(expected_screen_name, Tweet())
+
+    mock_path.isdir.assert_called_with(tweet_catch.cache_path)
+    mock_makedirs.assert_called_with(tweet_catch.cache_path)
 
 """
 
