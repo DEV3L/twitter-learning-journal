24ce2b207e5054f70f31d2d08ad986e5c1b483b7
Justin Beall <jus.beall@gmail.com>
2018-01-21 18:57:19 -0500

    Add seem for sub classification of Tweets

--- app/twitter_learning_journal/services/tweets_processing_service.py
+++ app/twitter_learning_journal/services/tweets_processing_service.py
@@ -4,12 +4,23 @@ from app.twitter_learning_journal.transformers.transform_str import remove_ignor
 
 
 class TweetsProcessingService:
-    def __init__(self, tweets: list, *, classification_model=None, weight_text=1, weight_hashtag=3):
+    sub_classification_factor = 10
+
+    def __init__(self, tweets: list, *, classification_model=None,
+                 weight_text: float = 1.0, weight_hashtag: float = 3.0):
         # reference to global static state... not ideal
         self.classification_model = get_classification_model(classification_model)
         self.tweets = tweets
         self.weight_text = weight_text
         self.weight_hashtag = weight_hashtag
+
+    @property
+    def sub_weight_text(self):
+        return self.weight_text / self.sub_classification_factor
+
+    @property
+    def sub_weight_hashtag(self):
+        return self.weight_hashtag / self.sub_classification_factor
 
     def count_tweet_words(self):
         for tweet in self.tweets:
@@ -17,9 +28,32 @@ class TweetsProcessingService:
             word_count = count_tokens(_full_text)
             tweet.word_count = word_count
 
-    def classify_tweets(self):
-        for tweet in self.tweets:
-            TweetClassifier(tweet, classification_model=self.classification_model).classify()
+    def classify_tweets(self, *, is_sub_classify=False):
+        [TweetClassifier(tweet, classification_model=self.classification_model).classify()
+         for tweet in self.tweets]
+
+        if is_sub_classify:
+            self._sub_classify_tweets()
+
+    def _sub_classify_tweets(self):
+        sub_classification_model = self._create_sub_classification_model()
+
+        unclassified_tweets = [tweet for tweet in self.tweets if not tweet.classification]
+        tweets_processing_service = TweetsProcessingService(unclassified_tweets,
+                                                            classification_model=sub_classification_model,
+                                                            weight_text=self.sub_weight_text,
+                                                            weight_hashtag=self.sub_weight_hashtag)
+        tweets_processing_service.classify_tweets()
+
+    def _create_sub_classification_model(self):
+        classified_tweets = [tweet for tweet in self.tweets if tweet.classification]
+        sub_classification_model = dict(get_classification_model(None))
+
+        for tweet in classified_tweets:
+            cleaned_full_text = remove_ignore_characters_from_str(tweet.full_text)
+            updated_classification = sub_classification_model[tweet.classification].union(cleaned_full_text)
+            sub_classification_model[tweet.classification] = updated_classification
+        return sub_classification_model
 
 
 def count_tokens(input_str):
