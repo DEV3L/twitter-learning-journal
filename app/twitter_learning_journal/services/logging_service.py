8ada91da05696dce212957d40698223ed7e95f74
Justin Beall <jus.beall@gmail.com>
2018-01-28 14:01:18 -0500

    Add logging service

---
+++ app/twitter_learning_journal/services/logging_service.py
@@ -0,0 +1,49 @@
+import logging
+import sys
+
+from app.twitter_learning_journal.dao.os_env import os_environ
+
+default_level = os_environ('DEFAULT_LOGGING_LEVEL', default=logging.INFO)
+default_logging_format = os_environ('DEFAULT_LOGGING_FORMAT',
+                                    default='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+class LoggingService:
+    def __init__(self, name: str, *, logging_format: str = default_logging_format, level: int = default_level):
+        self.name = name
+        self.logging_format = logging_format
+        self.level = level
+
+        self._logger = None
+
+    @property
+    def logger(self):
+        if self._logger:
+            return self._logger
+
+        self._logger = self._build_logger()
+        return self._logger
+
+    def log_message(self, message):
+        self.logger.info(message)
+
+    def log_exception(self, message):
+        self.logger.exception(message)
+
+    def _build_logger(self):
+        logger = logging.getLogger(self.name)
+        logger.setLevel(self.level)
+
+        sys_stdout_stream_handler = self._build_stream_handler(sys.stdout)
+        logger.addHandler(sys_stdout_stream_handler)
+
+        return logger
+
+    def _build_stream_handler(self, stream):
+        formatter = logging.Formatter(self.logging_format)
+
+        stream_handler = logging.StreamHandler(stream)
+        stream_handler.setLevel(self.level)
+        stream_handler.setFormatter(formatter)
+
+        return stream_handler
