492cf4df8ea0c766ac759db878e1eff7fa49dd60
DEV3L <jus.beall@gmail.com>
2018-07-28 13:15:45 -0400

    Alter conference

--- app/twitter_learning_journal/retrievers/details/conferences.py
+++ app/twitter_learning_journal/retrievers/details/conferences.py
@@ -1,8 +1,8 @@
 def get_conferences(tweets):
     conferences = [tweet for tweet in tweets
                    if tweet.full_text.lower().startswith("i'm at")
-                   or tweet.full_text.lower().startswith('attending:')
-                   or tweet.full_text.lower().startswith('attended:')
+                   or tweet.full_text.lower().startswith('attending')
+                   or tweet.full_text.lower().startswith('attended')
                    or tweet.full_text.lower().startswith('extreme fake it till you make it')
                    ]
 
